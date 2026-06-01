41c53c8667f465bfc3a556c0f01103af9d76cd7c
Marcus Vinícius Rocha Marçal <36481692+marcusmarcal@users.noreply.github.com>
2026-06-01 10:11:10 +0100

    Kiosk: trying to remove mouse cursor #2

--- srt-push.py
+++ srt-push.py
@@ -11,7 +11,7 @@ import shutil
 # CONFIG
 # ============================================================
 
-HTML_URL = "https://127.0.0.1/id3as-DC-Monitor.html?view=nodes&dc=ix&sort=nW&dir=-1&inuse=1"
+HTML_URL = "https://127.0.0.1/id3as-DC-Monitor.html?view=nodes&dc=ix&inuse=1&sort=nW&dir=1"
 
 SRT_URL = "srt://10.11.203.1:3292?mode=caller&latency=1000&passphrase=rQ6zgFnfz1WgmJ0AgzI4Zs7Own54K0dU"
 
