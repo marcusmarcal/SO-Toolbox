e5541c211cba2ef5b5fe865695df98fbfd94604b
Marcus Vinícius Rocha Marçal <36481692+marcusmarcal@users.noreply.github.com>
2026-08-27 10:31:24 +0100

    MANUAL: disabled #routes_rota.register_routes(app)

--- proxy.py
+++ proxy.py
@@ -923,7 +923,7 @@ def server_stats():
 routes_auth.register_routes(app)
 routes_gop.register_routes(app)
 
-routes_rota.register_routes(app)
+#routes_rota.register_routes(app)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050, threaded=True)
